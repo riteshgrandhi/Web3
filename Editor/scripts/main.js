a8d80f1d3702f42c03bdddb275485f564a2608c7
riteshgrandhi <ritesh.grandhi@gmail.com>
2017-01-20 12:59:09 +0530

    mouse clicks
    Raycaster setup, faulty but functional

--- Editor/scripts/main.js
+++ Editor/scripts/main.js
@@ -1,14 +1,16 @@
-var scene, renderer, camera, controls;
-var selectionBox;
+var scene, renderer, camera, controls,grid;
+var selectionBox,selectionControls;
 var WIDTH = 800, HEIGHT = 600;
 var selectedObjects=[];
 
 var clock = new THREE.Clock();
 
 init();
+//window.addEventListener('load', init);
 animate();
 
 function init(){
+	document.body.addEventListener( 'mouseup', onEditorMouseClick, false);
 
 	renderer = new THREE.WebGLRenderer({/*antialias:true*/});
 	
@@ -27,24 +29,31 @@ function init(){
 	canvasElem.appendChild(renderer.domElement);
 
 	controls = new THREE.OrbitControls( camera, canvasElem );
-	controls.addEventListener( 'change', render );
+	selectionControls = new THREE.TransformControls( camera, canvasElem);
+	scene.add(selectionControls);
 
-	var grid = new THREE.GridHelper(10,10,0x444444,0x666666);
+	controls.addEventListener( 'change', render );	
+	selectionControls.addEventListener( 'change', render );
+
+	grid = new THREE.GridHelper(10,20,0x444444,0x666666);
 	scene.add(grid);
 
 }
 function render(){
+	if(selectionBox!==undefined){
+		selectionBox.update(selectedObjects[0]);
+	}
 	renderer.render(scene, camera);
 }
 function animate(){
+	updateViewportControls();
 	requestAnimationFrame(render);
-	controls.update();
 }
 function addNewObj(){
 	var obj = new THREE.Mesh(new THREE.BoxBufferGeometry(1,1,1), new THREE.MeshBasicMaterial({color: 0xffffbb}));
-	obj.position.set(Math.random()*6-3,Math.random()*6-3,Math.random()*6-3);
 	scene.add(obj);
-	obj.name="lol";
+	obj.name="haha";
+
 	if(selectionBox == undefined){
 		selectionBox = new THREE.BoxHelper(obj, 0xff0000);
 		scene.add(selectionBox);
@@ -52,9 +61,48 @@ function addNewObj(){
 		selectionBox.update(obj);
 	}
 	
-	selectedObjects = [];
-	selectedObjects.push(obj);
-	
+	selectObject(obj);
 	document.getElementById("menu").innerHTML += selectedObjects[0].name;
 	animate();
+}
+
+function updateViewportControls(){
+	controls.update();
+	//selectionControls.update();
+}
+
+function onEditorMouseClick(event){
+	event.preventDefault();
+	console.log("up");
+	var vector = new THREE.Vector3();
+	var raycaster = new THREE.Raycaster();
+	var dir = new THREE.Vector3();
+	var mouse = new THREE.Vector2();
+	mouse.x = (( event.clientX / WIDTH ) * 2 - 1.014);
+	mouse.y = - ( event.clientY / HEIGHT ) * 2 + 1 + 0.054;
+
+	if ( camera instanceof THREE.OrthographicCamera ) {
+		vector.set( ( event.clientX / WIDTH ) * 2 - 1, - ( event.clientY / HEIGHT ) * 2 + 1, - 1 ); // z = - 1 important!
+		vector.unproject( camera );
+		dir.set( 0, 0, - 1 ).transformDirection( camera.matrixWorld );
+		raycaster.set( vector, dir );
+	} else if ( camera instanceof THREE.PerspectiveCamera ) {
+		raycaster.setFromCamera(mouse, camera);
+		//scene.add(new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 100, Math.random() * 0xffffff ));
+		render();
+	}
+
+	var intersects = raycaster.intersectObjects( scene.children );
+	if(intersects.length > 0){
+		selectObject(intersects[0].object);
+	}
+}
+
+function selectObject(obj){
+	if(obj!=grid){
+		selectedObjects = [];
+		selectedObjects.push(obj);
+		selectionControls.attach(selectedObjects[0]);
+		render();
+	}
 }
