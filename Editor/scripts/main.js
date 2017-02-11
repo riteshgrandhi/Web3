673c107d6ac69fa1a35c769b367006a9a4ae2c0a
riteshgrandhi <ritesh.grandhi@gmail.com>
2017-02-12 01:15:27 +0530

    Major UI and internal upgrades
    added initial OBJ import support, integrated Editor window into UI, needs better UI rescale compatibility

--- Editor/scripts/main.js
+++ Editor/scripts/main.js
@@ -1,21 +1,25 @@
 var scene, renderer, camera, controls,grid;
 var userObjects = [];
 var selectionBox,selectionControls;
-var WIDTH = 800, HEIGHT = 600;
 var selectedObjects=[];
+var canvasElem = document.getElementById( "viewport" );
+var WIDTH = canvasElem.getBoundingClientRect().width;
+var HEIGHT = canvasElem.getBoundingClientRect().height;
 
 var simulatePhysics = false;
 var clock = new THREE.Clock();
 Physijs.scripts.worker = 'scripts/Physijs/physijs_worker.js';
 Physijs.scripts.ammo = 'ammo.js';
 
-init();
-//window.addEventListener('load', init);
-animate();
+window.onload = function(){
+	init();
+	animate();
+}
 
 function init(){
-	var canvasElem = document.getElementById( "viewport" );
+	//canvasElem = document.getElementById( "viewport" );
 	canvasElem.addEventListener( 'mouseup', onEditorMouseClick, false );
+	//window.addEventListener( 'resize', onCanvasResize, false);
 	window.addEventListener( 'keydown', onEditorKeyDown, false );
 	window.addEventListener( 'keyup', onEditorKeyUp, false );
 
@@ -42,7 +46,7 @@ function init(){
 	selectionControls = new THREE.TransformControls( camera, canvasElem);
 	scene.add(selectionControls);
 
-	controls.addEventListener( 'change', render );	
+	controls.addEventListener( 'change', render );
 	selectionControls.addEventListener( 'change', render );
 
 	grid = new THREE.GridHelper( 10, 20, 0x444444, 0x666666 );
@@ -54,6 +58,7 @@ function render(){
 		selectionBox.update(selectedObjects[0]);
 	}
 	renderer.render( scene, camera );
+	selectionControls.update();
 	if(simulatePhysics){
 		scene.simulate();
 	}
@@ -64,14 +69,24 @@ function animate(){
 	requestAnimationFrame( animate );
 	//animate();
 }
-function addNewObj(){
-	//var obj = new THREE.Mesh(new THREE.BoxBufferGeometry(1,1,1), new THREE.MeshBasicMaterial({color: 0xffffbb}));
-	//var k = parseInt(window.prompt("Enter Mass"));
-	var obj = new Physijs.BoxMesh(new THREE.BoxBufferGeometry(1,1,1), new THREE.MeshBasicMaterial({color: 0xffffbb}));
+function addNewObj(type){
+	var obj;
+	switch(type){
+		case 1: obj = new Physijs.BoxMesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 0xffffbb}));
+				break;
+		case 2: obj = new Physijs.CylinderMesh(new THREE.CylinderGeometry(0.5, 0.5, 1, 16), new THREE.MeshBasicMaterial({color: 0xffffbb}));
+				break;
+		case 3: obj = new Physijs.SphereMesh(new THREE.SphereGeometry(0.5, 12, 8), new THREE.MeshBasicMaterial({color: 0xffffbb}));
+				break;
+		case 4: addCustomObj(new THREE.MeshBasicMaterial({color: 0xffffbb}));
+					return;
+				break;
+	}
 	scene.add(obj);
 	obj.__dirtyPosition = true;
 	userObjects.push(obj);
 	obj.name="object." + userObjects.length;
+	updateList();
 
 	if(selectionBox == undefined){
 		selectionBox = new THREE.BoxHelper(obj, 0xff0000);
@@ -83,6 +98,7 @@ function addNewObj(){
 	selectObject(obj);
 	//document.getElementById( "menu" ).innerHTML += selectedObjects[0].name;
 	animate();
+	return false;
 }
 
 function updateViewportControls(){
@@ -91,16 +107,20 @@ function updateViewportControls(){
 }
 
 function onEditorMouseClick(event){
-	var xfac = -0.022;
-	var yfac = 0.095;
+	var xfac = 0;//-0.022;
+	var yfac = 0;//0.095;
 	event.preventDefault();
 	//console.log("up");
+	var rect = canvasElem.getBoundingClientRect();
+
 	var vector = new THREE.Vector3();
 	var raycaster = new THREE.Raycaster();
 	var dir = new THREE.Vector3();
 	var mouse = new THREE.Vector2();
-	mouse.x = ( event.clientX / WIDTH ) * 2 - 1 + xfac;
-	mouse.y = - ( event.clientY / HEIGHT ) * 2 + 1 + yfac;
+	/*mouse.x = ( event.clientX / WIDTH ) * 2 - 1 + xfac;
+	mouse.y = - ( event.clientY / HEIGHT ) * 2 + 1 + yfac;*/
+	mouse.x = (( event.clientX - rect.left ) / rect.width) * 2 - 1;
+	mouse.y = -(( event.clientY - rect.top ) / rect.height) * 2 + 1;
 
 	if ( camera instanceof THREE.OrthographicCamera ) {
 		vector.set( ( event.clientX / WIDTH ) * 2 - 1, - ( event.clientY / HEIGHT ) * 2 + 1, - 1 ); // z = - 1 important!
@@ -113,7 +133,7 @@ function onEditorMouseClick(event){
 		render();
 	}
 
-	var intersects = raycaster.intersectObjects( userObjects );
+	var intersects = raycaster.intersectObjects( userObjects, true );
 	if(intersects.length > 0){
 		if(intersects.length == 1){
 			selectObject(intersects[0].object);
@@ -136,13 +156,16 @@ function onEditorMouseClick(event){
 }
 
 function selectObject(obj){
-	if(obj!=grid){
-		selectedObjects = [];
-		selectedObjects.push(obj);
-		selectionControls.attach(obj);
-		document.getElementById("bottom").innerHTML = obj.name;
-		render();
+	while(obj.parent!=scene){
+		obj=obj.parent;
 	}
+	selectedObjects = [];
+	selectedObjects.push(obj);
+	selectionControls.attach(obj);
+	document.getElementById("objName").value = obj.name;
+	setColor(parseInt(obj.userData.ButtonId) , false);
+	document.getElementById("bottom").innerHTML = obj.name;
+	//render();
 }
 
 function onEditorKeyDown(event){
@@ -172,11 +195,113 @@ function setObjName(event){
 	var key = event.keyCode;
 	if(key == 13){
 		event.preventDefault();
-		var tempName = document.getElementById("sample").value;
+		var tempName = document.getElementById("objName").value;
 		if( selectedObjects.length > 0 ){
-			selectedObjects[0].name = tempName;
-			document.getElementById("sample").value = "";
+			if(tempName != ""){
+				selectedObjects[0].name = tempName;
+			}else{
+				document.getElementById("objName").value = selectedObjects[0].name;
+			}
+			document.getElementById("objName").blur();
+			updateList();
 			document.getElementById("bottom").innerHTML = selectedObjects[0].name;
 		}
 	}
+}
+
+function onCanvasResize(){
+	//console.log("before"+WIDTH);
+	/*WIDTH = 0.5913 * window.innerWidth; 
+	HEIGHT = 0.7 * window.innerHeight;*/
+	/*WIDTH = canvasElem.getBoundingClientRect().width;
+	HEIGHT = canvasElem.getBoundingClientRect().height;*/
+	//console.log("after"+WIDTH);
+	renderer.setSize( WIDTH, HEIGHT);
+	camera.aspect	= WIDTH / HEIGHT;
+	camera.updateProjectionMatrix();
+	canvasElem.width=WIDTH;
+	canvasElem.height=HEIGHT;
+	//console.log(canvasElem.width);
+}
+function updateList(){
+	var list = document.getElementById("objectList");
+	list.innerHTML = "";
+	for(var i=0;i<userObjects.length;i++){
+		list.innerHTML += "<input type='button' class='button' id='obj" + i + "' value = \""
+							+ userObjects[i].name + " \" onclick=\"setColor('" + i + "')\"/><br>";
+		userObjects[i].userData = { ButtonId: i };
+	}
+}
+function setColor(btn, setFromList = true){
+  var crnt=btn;  
+  var i=0;
+  len = userObjects.length;
+  while(i<len){
+  var property = document.getElementById("obj"+i);
+  //console.log(btn);
+    if (i != crnt){
+        property.style.backgroundColor = "#444444";   
+        property.style.color='lightgrey';
+        //property.style.border='1px hidden';
+    }
+    else{
+        property.style.backgroundColor = "grey";
+        property.style.color='white';
+        if(setFromList){
+        	selectObject(userObjects[i]);
+        }
+        //property.style.border='1px solid black';
+    }
+  	i++;
+	}
+}
+
+function addCustomObj(material){
+	var objGeo = new THREE.Geometry();
+	//var obj = new Physijs.BoxMesh(objGeo, material);
+	
+	var objPath = window.prompt("Enter Object Path(Web):");
+	var texPath = "";//window.prompt("Enter Texture Path(Web):");
+	var manager = new THREE.LoadingManager();
+				manager.onProgress = function ( item, loaded, total ) {
+					console.log( item, loaded, total );
+				};
+	var texture = new THREE.TextureLoader( manager ).load(texPath);
+    var loader  = new THREE.OBJLoader( manager ).load(objPath,  
+    	function ( object )
+        {
+        	var name;
+            object.traverse(
+                function ( child )
+                {
+                    if(child instanceof THREE.Mesh)
+                    {
+                    	child.material = material;
+						//child.material.map = texture;
+						name = child.name;
+					}
+				} );
+			scene.add(object);
+			userObjects.push(object);
+			object.name = name == '' ? "object." + userObjects.length : name;
+			updateList();
+			if(selectionBox == undefined){
+				selectionBox = new THREE.BoxHelper(object, 0xff0000);
+				scene.add(selectionBox);
+			}else{
+				selectionBox.update(object);
+			}
+			selectObject(object);
+			animate();
+		},
+		function ( xhr ) {
+			if ( xhr.lengthComputable ) {
+				var percentComplete = xhr.loaded / xhr.total * 100;
+				console.log( Math.round(percentComplete, 2) + '% downloaded' );
+				}
+			},
+			function ( xhr ) {
+				window.alert("failed to load resource from " + objPath);
+				return null;
+			} );
 }
